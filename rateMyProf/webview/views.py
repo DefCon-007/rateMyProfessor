d0f9b243109ae1e4255ca4f60864903d5f0df5fb
Ayush Goyal <ayushgoyal.iitkgp@gmail.com>
2018-12-29 16:57:19 +0530

    Adds basic html

--- rateMyProf/webview/views.py
+++ rateMyProf/webview/views.py
@@ -1,3 +1,17 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request) :
+    return render(request, 'webview/index.html')
+
+
+def sendPassword(request) :
+    print(1)
+    email = request.GET.get('email', '')
+    if email is not "" :
+
+        print(email)
+        return JsonResponse({"login" : "login", "avatar_url" : "url"}, status=200)
+        return JsonResponse({"login": "login", "avatar_url": "url"}, status=500)
