f277afa2613f1db9fb31f13cb180e12ad16a320e
Ayush Goyal <ayushgoyal.iitkgp@gmail.com>
2018-12-30 16:19:51 +0530

    Adds allowed hosts in config file

--- rateMyProf/rateMyProf/settings.py
+++ rateMyProf/rateMyProf/settings.py
@@ -14,7 +14,7 @@ import os
 import configparser
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
-
+import json
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -29,8 +29,7 @@ SECRET_KEY = config.get("core", "SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config.getboolean("core", "DEBUG")
-
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config.get("core", "ALLOWED_HOSTS").split(",")
 
 # Application definition
 
